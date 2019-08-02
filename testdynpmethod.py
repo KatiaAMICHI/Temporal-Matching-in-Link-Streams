d03c3451494b3d69f35e6f4592f02b93e228f84f
Katia Amichi <amichi.katia@gmail.com>
2019-08-02 20:20:58 +0200

    beginning of the decomposition method and it misses the sort of the position list for maxgammaMatching1D

--- testdynpmethod.py
+++ testdynpmethod.py
@@ -60,41 +60,6 @@ def matchingDP(n, t_max, d, pos, file):
         os.remove(file)
 
 
-def mathing_dp_2D(file):
-    with open(file, 'r') as f:
-        n, y, d = list(map(int, f.readline().split()))
-        i = 0
-        x = [[0] * (n + 1)] * (y + 1)
-
-        for line in f.readlines():
-            print(line)
-            x[i] = list(map(int, line.split(",")))
-            i += 1
-        print("************print X *********************")
-        pprint.pprint(x)
-        print("**************************************")
-        print(machingDP_t_2D(int(n), int(y), d, x))
-        print("**************************************")
-
-
-def machingDP_t_2D(n, y, d, xInput):
-    x = xInput.copy()
-
-    M = [[0] * (n + 1)] * (y + 1)
-
-    for i in range(2, y + 1):
-        for j in range(2, n + 1):
-
-            if abs(x[i][j] - x[i - 1][j]) <= d and abs(x[i][j - 1] - x[i - 1][j - 1]) <= d:
-                M[i][j] = M[i - 2][j] + 1
-                print("M[", i, "][", j, "]=", M[i][j])
-            else:
-                M[i][j] = M[i - 1][j]
-
-    print("!!!!!!!!!!!!!!!!!!!!!")
-    pprint.pprint(M)
-
-
 def mathing_dp(file):
     with open(file, 'r') as f:
         n, d = list(map(int, f.readline().split()))
@@ -134,6 +99,8 @@ def matchingDP_t(n, d, xInput):
 
 def genGammaEdges():
     path = "/home/katia/Bureau/testbed/tests/"
+
+    result = []
     for file in os.listdir(path):
         file_output = path + file.replace(".position", ".nb_matching")
         if file.endswith('.position'):
@@ -149,57 +116,51 @@ def genGammaEdges():
                         t = int(t)
                         x[t] = list(map(int, pos.replace("]", "").replace(",", " ").split()))
                         M[t], edges = matchingDP_t(n, d, [0] + x[t])
+                        result.append((M[t][-1], edges))  # ajout du tuple (nb_matching, [les matching])
                         f_outPut.write(str(M[t][-1]) + " " + str(edges) + "\n")
                         # f_outPut.write(str(t) + " " + str(x[t]) + " " + str(M[t][-1]) + " " + str(edges) + "\n")
                 pprint.pprint(M)
+    return result
 
 
-def gammaMatchig1DV0(n, tmax, d, xInput):
-    print(",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,")
-    """
+def max_t_matching(list_pos_matching):
+    max_matching = (0, [])
+    idx_max_matching = -1
+    i = 0
+    for x in list_pos_matching:
+        if max_matching[0] < x[0]:
+            max_matching = x
+            idx_max_matching = i
+        i += 1
+    return max_matching, idx_max_matching
 
-        :param n: nb sommet
-        :param d: distance
-        :param xInput: toutes les positions pour chaque sommets pour chaque t
-        :return:
-        """
 
-    M = np.zeros((n + 1, tmax + 1)).astype(int)
+def update_list_pos_matching(list_pos_matching, max_matching, idx_max_matching):
+    for edge in max_matching:
+        for i in range(idx_max_matching - gamma + 1, idx_max_matching + gamma - 1):
+            for elem in list_pos_matching[i]:
+                for edge_p in elem[1]:
+                    if edge_p[0] == edge[0] or edge_p[1] == edge[0] or edge_p[0] == edge[1] or edge_p[1] == edge[1]:
+                        elem[1].remove(edge_p)
+                        elem[0] -= 1
+    list_pos_matching.remove(max_matching)
+    return list_pos_matching
 
-    x = xInput.copy()
 
-    for i in range(2, n + 1):
-
-        M[i][1] = M[i - 1][tmax]
-
-        print("i : ", i)
-        print("        <<< M[", i, "][", 0, "] : ", M[i][0])
-        print("        <<< M[", i, "][", 1, "] : ", M[i][1])
-        # x[i].sort()
-        for j in range(2, tmax + 1):
-            print("     j : ", j)
-            print("    x[", i, "][", j - 1, "] - x[", i - 1, "][", j - 1, " ] : ", x[i][j - 1] - x[i - 1][j - 1])
-            print("    x[", i, "][", j - 1, "] : ", x[i][j - 1], " - x[", i - 1, "][", j - 1, " ] : ", x[i - 1][j - 1])
-            if abs(x[i][j] - x[i - 1][j]) <= d and abs(x[i][j - 1] - x[i - 1][j - 1]) <= d:
-                # ok
-                M[i][j] = max(M[i - 2][j - 2], M[i - 2][j]) + 1
-                print("        [IF] M[", i, "][", j, "] : ", M[i][j])
-                print("        [IF](i-2,j-2) M[", i - 2, "][", j - 2, "] : ", M[i - 2][j - 2])
-            else:
-                # ko
-                M[i][j] = M[i - 1][j - 1]
-                print("         [else] M[", i, "][", j, "] : ", M[i][j])
-        M[i][0] = M[i][tmax]
-        print("******************print M*********************")
-        pprint.pprint(M)
-        print("**********************************************")
-    return M
+def nb_gamma_matching_decomposition(list_pos_matching, nb_matching, M):
+    # TODO ajout un point d'arret !!!!!
+    max_matching, idx_max_matching = max_t_matching(list_pos_matching)
+    M.append(max_matching[1])  # ajout des gamma_matching
+    nb_matching += max_matching[0]
+    update_list_pos_matching(list_pos_matching, max_matching, idx_max_matching)
+    nb_gamma_matching_decomposition(list_pos_matching[0:idx_max_matching], nb_matching, M)
+    nb_gamma_matching_decomposition(list_pos_matching[idx_max_matching::])
 
 
 def gammaMatchig1D(n, tmax, d, xInput):
     print(",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,")
     """
-
+        OK
         :param n: nb sommet
         :param d: distance
         :param xInput: toutes les positions pour chaque sommets pour chaque t
@@ -234,7 +195,7 @@ def gammaMatchig1D(n, tmax, d, xInput):
                 print("1 **************** B *******************")
                 pprint.pprint(B)
                 print("**************************************")
-                bool_inter, nb_inter, nb_matching_b, Bp = intersection(A, B.copy(), j, i, i - 1, nb_matching_b)
+                bool_inter, nb_inter, nb_matching_b, Bp = intersectionA(A, B.copy(), j, i, i - 1, nb_matching_b)
                 if bool_inter:
                     # si y a une intersection alors on fait ce qu'on doit faire ici
                     if j - 2 == 0:
@@ -287,21 +248,26 @@ def gammaMatchig1D(n, tmax, d, xInput):
     return M
 
 
-# TODO faut aussi vérifier si le truc est dnas B
-
-def intersection(A, Bp, t, u, v, nb_matching_b):
+def intersectionA(A, Bp, t, u, v, nb_matching_b):
+    """
+    OK
+    :param A:
+    :param Bp:
+    :param t:
+    :param u:
+    :param v:
+    :param nb_matching_b:
+    :return:
+    """
     gamma = 2
     nb_inter = 0
     bool_inter = False
     B = copy.deepcopy(Bp)
-    print("Bp:", Bp)
     for i in range(t - gamma + 1, t + gamma):
         if i not in range(len(A)):
             break
         for edge in A[i]:
             if u in edge or v in edge:
-                print("t:", t, " edge : ", edge)
-                print("u:", u, "v:", v)
                 bool_inter = True
                 if t in range(t - gamma - 1, t - 1) or (u > edge[0] and v == edge[0]) or (u == edge[0] and v > edge[0]):
                     if edge in B[i]:
@@ -309,11 +275,18 @@ def intersection(A, Bp, t, u, v, nb_matching_b):
                         nb_matching_b -= 1
                     nb_inter += 1
     print(">>>>>>>>>>>>>>>>>>>>>>><<<<<<<><<<<>><<<<<<<>> nb_inter :", nb_inter)
-    print("Bp:", Bp)
     return bool_inter, nb_inter, nb_matching_b, B
 
 
 def intersectionB(B, t, u, v):
+    """
+    Ok
+    :param B:
+    :param t:
+    :param u:
+    :param v:
+    :return:
+    """
     gamma = 2
 
     # on vérifie sur t-1 que par ce que ici on travaille avec gamma=2
@@ -333,6 +306,12 @@ def intersectionB(B, t, u, v):
 
 
 def unzip(ls):
+    """
+    OK
+    Pour trier une list et save les indexs
+    :param ls:
+    :return:
+    """
     if isinstance(ls, list):
         if not ls:
             return [], []
@@ -345,6 +324,10 @@ def unzip(ls):
 
 
 def test_unzip():
+    """
+    OK
+    :return:
+    """
     xs = [4, 2, 6, 9, 0]
     print("AVANT arr : ", xs)
     i_xs = [(x, i) for (i, x) in enumerate(xs)]
@@ -369,8 +352,8 @@ if __name__ == '__main__':
         print("*********************print x*******************************")
         pprint.pprint(x)
         print("***********************************************************")
-        # pprint.pprint(gammaMatchig1D(n, tmax, d, x))
-        gammaMatchig1D(n, tmax, d, x)
+        # gammaMatchig1D(n, tmax, d, x)
+    genGammaEdges()
 
 # xMax=20
 # d=1
