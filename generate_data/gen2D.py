ee669137c49034b48090a7cca93fe7c64622fcc0
KatiaAmichi <amichi.katia@gmail.com>
2019-08-14 17:10:23 +0200

    add method for generation 2D graph

---
+++ generate_data/gen2D.py
@@ -0,0 +1,23 @@
+import random
+import sys
+
+n = int(sys.argv[1])
+T = int(sys.argv[2])
+d = int(sys.argv[3])
+
+xposition = [0] * n
+
+for t in range(T):
+    print(t, xposition)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if abs(xposition[i] - xposition[j]) <= d:
+                print(t, i, j)
+    for i in range(n):
+        var_to_add = random.randint(1, 20) + i % n * random.randint(1, 15)
+        var_to_sud = random.randint(1, 20) + i % n * random.randint(1, 15)
+
+        if random.randint(0, 4) < 1 and xposition[i] > 10:
+            xposition[i] -= random.randint(1, 30)
+        else:
+            xposition[i] += random.randint(0, 80)
